df38a9f9a537b3d74ce2ecfeee4715dfa876b35a
Baghatraj N <baghatraj484@gmail.com>
2023-10-26 11:21:39 +0530

    Backend changes

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
-dotenv.config({path : '../.env'})
+dotenv.config()
 
 export const port = 5000;
 
-export const mongodbURL = process.env.MongoDb_URI;
+export const mongodbURL = process.env.MONGODB_URI;
 
 // "mongodb://127.0.0.1:27017/Books" - localhost
